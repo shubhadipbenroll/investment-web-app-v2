704f36ac3ca034f0356fd184f0c1ae221ce5a14b
shubhadipbenroll <shubhadipbera.cert@gmail.com>
2024-09-24 08:49:19 +0000

    Update code

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
-from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+def investment_app():
+  return render_template('home.html')
+
+
+@app.route("/login")
+def login_page():
+  return render_template('login-page.html')
+
+
+@app.route("/register")
+def user_register():
+  return render_template('user-register.html')
+
+
+@app.route("/dashboard")
+def dashboard_page():
+  return render_template('dashboard.html')
+
+
+@app.route("/ticker")
+def create_ticker():
+  return render_template('ticker-create.html')
+
+
+@app.route("/showtickers")
+def show_tickers():
+  return render_template('show-tickers.html')
+
+
+@app.route("/showusers")
+def show_users():
+  return render_template('show-users.html')
 
 
 print(__name__)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port='5000', debug=True)
